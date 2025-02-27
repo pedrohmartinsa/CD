4d4d3ca3b839e7abafa70dc90a67b74a7b18dbac
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2025-02-27 11:54:52 -0300

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
+#Para carregar arquivo para dataframe Pandas (no formato csv)
 dados = pd.read_csv("Credit.csv")
 
-dados.shape()
+#Para ver o "corpo" da tabela
+dados.shape
 
+#retorna um resumo estatístico de colunas númericas
+dados.describe()
 
+#retorna as primeiras linhas da tabela (aceita parâmetro)
+dados.head(6)
+
+#retorna as últimas linhas da tabela (aceita paramêtro)
+dados.tail(3)
